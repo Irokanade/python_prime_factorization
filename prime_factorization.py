07810b64ac249f75b9d7bbd66748549236271717
Irokanade <67633095+Irokanade@users.noreply.github.com>
2021-03-25 02:01:07 +0800

    initial commit

---
+++ prime_factorization.py
@@ -0,0 +1,30 @@
+import math
+
+a = int(input('input an integer: '))
+
+for i in range(2, int(math.sqrt(a)+1)):
+    if a%i == 0:
+        print(f'{a} is not a prime')
+        break
+
+    else:
+        print(f'{a} is a prime')
+        exit()
+
+print('{0}='.format(a), end = "")
+
+firstFactorPrinted = bool(False)
+
+for i in range(2, int(math.sqrt(a)+1)):
+    while a%i==0:
+        if firstFactorPrinted == False:
+            print('{0}'.format(i), end = "")
+            firstFactorPrinted = True
+        
+        else:
+            print('*{0}'.format(i), end = "")
+            a=a/i
+
+    
+if a > 2:
+        print('*{0}'.format(i))
